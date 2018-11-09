282b101d0b4cf7fce577bbe564de207613c0d2b9
unknown <wengyunbing@gmail.com>
2018-11-09 08:08:47 -0500

    with phys ass

---
+++ test.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void main(){
+     for(int i= 1; i<= 5; ++i ) {   for (int j = 1; j <= 3; ++j ) {   for (int k = 1; k <= 4; ++k )  printf( "%s","*");   puts( "");  }  puts( "");    }
+}
